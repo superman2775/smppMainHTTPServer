4eed4945ed41263d0ce7feb13cd095a2baae8ff4
frickingbird8002 <sakul8002@icloud.com>
2025-04-07 00:52:52 +0200

    fixed mobile compat for menus

--- content/js/main.js
+++ content/js/main.js
@@ -19,11 +19,27 @@ menuButtons.forEach(button => {
     })
 })
 
+function openMobileMenu() {
+    document.getElementById("mobile-menu").classList.add("mobile-menu-open")
+    document.getElementById("overlay").classList.add("active")
+}
+
+function closeMobileMenu() {
+    document.getElementById("mobile-menu").classList.remove("mobile-menu-open")
+    document.getElementById("overlay").classList.remove("active")
+}
+
+document.getElementById("mobile-menu-button").addEventListener("click", openMobileMenu)
+document.getElementById("mobile-menu-close-button").addEventListener("click", closeMobileMenu)
+
 document.addEventListener("click", function (e) {
     if (!e.target.closest('.top-nav-menu-button')) {
         const menuButtons = document.querySelectorAll('.top-nav-menu-button');
         menuButtons.forEach(button => {
             button.classList.remove("open-menu")
         })
+    }
+    if (!e.target.closest('#mobile-menu') && !e.target.closest('#mobile-menu-button')) {
+        closeMobileMenu()
     }
 })
